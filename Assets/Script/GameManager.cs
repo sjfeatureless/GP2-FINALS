0c8a3b45801215e24fdef371d66601f9e1d77fc0
sjfeatureless <134472212+sjfeatureless@users.noreply.github.com>
2023-11-21 00:09:55 +0800

    added prologue and intro
    working on it

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -5,19 +5,38 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject TitleScreen;
+    public GameObject UI_BG, Prologue, Prologue2, Prologue3, Introduction, Exit, Title, Bg_1, Play;
+    public int health, stamina;
+    public TextMeshProUGUI healthText, staminaText;
     // Start is called before the first frame update
     void Start()
     {
-        TitleScreen.SetActive(true);
+        UI_BG.SetActive(true);
     }
 
     // Update is called once per frame
 
     public void StartGameButton()
     {
-        TitleScreen.SetActive(false);
+        Play.SetActive(false);
+        Bg_1.SetActive(false);
+        Title.SetActive(false);
+        Exit.SetActive(false);
+        Introduction.SetActive(true);
     }
-    
-
+    public void IntroStart()
+    {
+        Introduction.SetActive(false);
+        Prologue.SetActive(true);
+    }
+    public void PrologueNext()
+    {
+        Prologue.SetActive(false);
+        Prologue2.SetActive(true);
+    }
+    public void PrologueNext2()
+    {
+        Prologue2.SetActive(false);
+        Prologue3.SetActive(true);
+    }
 }
